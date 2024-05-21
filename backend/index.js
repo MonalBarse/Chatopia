d02403fd599437d8cd003e6d06f14936b1289553
MonalBarse <monal.barse.cer22@itbhu.ac.in>
2024-05-21 15:18:13 +0530

    Refactor: Testing Deployment

--- backend/index.js
+++ backend/index.js
@@ -69,7 +69,7 @@ const server = app.listen(PORT, () => {
 const io = require('socket.io')(server, {
   pingTimeout: 60000,
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://chatopia-f70q.onrender.com/',
   },
 });
 
